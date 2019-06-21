fc86fd56ca2b1f97c5f583a0a978b7a9c0a0cba4
kliuchyk <kliuchyk@gmail.com>
2019-06-21 14:43:34 +0300

    add app header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BookComponent } from './components/book/book.component';
 import { BooksListComponent } from './components/books-list/books-list.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookComponent,
-    BooksListComponent
+    BooksListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
