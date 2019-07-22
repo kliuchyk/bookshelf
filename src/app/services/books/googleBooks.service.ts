e89db778e5e1ac4b537ef1a741f406c935807ba8
kliuchyk <kliuchyk@gmail.com>
2019-07-22 19:19:21 +0300

    rename servcie

--- src/app/services/books/googleBooks.service.ts
+++ src/app/services/books/googleBooks.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class GoogleService {
+export class GoogleBooksService {
 
   constructor(private http: HttpClient) { }
 
